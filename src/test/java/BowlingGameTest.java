861919284d4b625356cbef7a4ba88f7467e854a0
Dan Neumann <dkneuma@gmail.com>
2022-04-14 17:16:53 -0400

    Add second kata start

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,19 +1,30 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class BowlingGameTest {
     @Test
-    public void testNewGame() throws Exception {
+    public void testNewGame()  {
         Game g = new Game();
         assertNotNull(g);
     }
 
     @Test
-    public void testGutterGame() throws Exception {
+    public void testGutterGame()  {
         Game g = new Game();
         for (int i = 0; i < 20; i++) {
             g.roll(0);
         }
+        assertEquals(0, g.score());
+    }
+
+    @Test
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
     }
 }
 
