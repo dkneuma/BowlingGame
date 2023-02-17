39ecd0c0b79a8a46cb009798a24d8448e44919db
Dan Neumann <dkneuma@gmail.com>
2023-02-17 11:06:25 -0500

    Added complete section with first Kata results

--- src/complete/test/java/BowlingGameTest.java
+++ src/complete/test/java/BowlingGameTest.java
@@ -8,5 +8,14 @@ public class BowlingGameTest {
         Game g = new Game();
         assertNotNull(g);
     }
+
+    @Test
+    public void testGutterGame() {
+        Game g = new Game();
+        for (int i = 0;i<20;i++) {
+            g.roll(0);
+        }
+        assertEquals(0, g.score());
+    }
 }
 
