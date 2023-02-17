91de7584c648dfb440aea25b03a8e18986588c9b
Dan Neumann <dkneuma@gmail.com>
2023-02-17 13:26:57 -0500

    Update to current status in SecondKata

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -6,7 +6,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
+    @Test
+    public void testNewGame()  {
+        Game g = new Game();
+        assertNotNull(g);
+    }
 
+    @Test
+    public void testGutterGame()  {
+        Game g = new Game();
+        int rolls = 20;
+        int pins = 0;
+        for (int i = 0; i < rolls; i++)
+            g.roll(pins);
+        assertEquals(0, g.score());
+    }
 
 }
 
